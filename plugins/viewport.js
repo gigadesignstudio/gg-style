b3bb50b9310ce36b32bff859ff7980da0976738f
ne-u <tommaso@ne-u.xyz>
2023-07-10 11:45:43 +0200

    mq ok

--- plugins/viewport.js
+++ plugins/viewport.js
@@ -1,22 +1,37 @@
-import mq from "../assets/js/viewport/mq";
-import viewport from "../assets/js/viewport";
 import tokens from "../assets/css/tokens.json";
 
+function setMq(mq, breakpoints) {
+  const breakpointsKeys = Object.keys(breakpoints);
+
+  breakpointsKeys.forEach((key) => {
+    mq.value[key] = window.matchMedia(
+      `(min-width: ${breakpoints[key]})`
+    ).matches;
+  });
+
+  return mq;
+}
+
 export default defineNuxtPlugin(() => {
+  const mq = ref(Object.keys(tokens.breakpoint).reduce((reducer, key) => {
+    return {
+      ...reducer,
+      [key]: false,
+    };
+  }, {}));
+
   onNuxtReady(() => {
+    setMq(mq, tokens.breakpoint)
+    window.onresize = () => {
+      setMq(mq, tokens.breakpoint)
+    };
+
     document.body.classList.add('loaded');
   });
 
   return {
     provide: {
-      mq: process.client
-        ? mq.value
-        : Object.keys(tokens.breakpoint).reduce((reducer, key) => {
-          return {
-            ...reducer,
-            [key]: false,
-          };
-        }, {}),
+      mq,
     },
   };
 });
