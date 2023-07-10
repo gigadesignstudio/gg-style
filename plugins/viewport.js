1d2f8a406b3f4a45dfa8f1900b934b35e2083ca1
ne-u <tommaso@ne-u.xyz>
2023-07-10 20:38:52 +0200

    prettier

--- plugins/viewport.js
+++ plugins/viewport.js
@@ -4,33 +4,29 @@ function setMq(mq, breakpoints) {
   const breakpointsKeys = Object.keys(breakpoints);
 
   breakpointsKeys.forEach((key) => {
-    mq.value[key] = window.matchMedia(
-      `(min-width: ${breakpoints[key]})`
-    ).matches;
+    mq.value[key] = window.matchMedia(`(min-width: ${breakpoints[key]})`).matches;
   });
 
   return mq;
 }
 
 export default defineNuxtPlugin(() => {
-  const mq = ref({});
+  const mq = ref(
+    Object.keys(tokens.breakpoint).reduce((reducer, key) => {
+      return {
+        ...reducer,
+        [key]: false,
+      };
+    }, {})
+  );
 
   onNuxtReady(() => {
-    if (process.client) {
-      setMq(mq, tokens.breakpoint)
-      window.onresize = () => {
-        setMq(mq, tokens.breakpoint)
-      };
-  
-      document.body.classList.add('loaded');
-    } else {
-      mq.value = Object.keys(tokens.breakpoint).reduce((reducer, key) => {
-        return {
-          ...reducer,
-          [key]: false,
-        };
-      }, {})
-    }
+    setMq(mq, tokens.breakpoint);
+    window.onresize = () => {
+      setMq(mq, tokens.breakpoint);
+    };
+
+    document.body.classList.add("loaded");
   });
 
   return {
