c8e28816092b16e14dedbfc9c26e588a411e4df8
Gabriele Leonardi <leonardi.gabriele.lg@gmail.com>
2023-07-17 15:53:14 +0200

    gg-style alias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
 const currentDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineNuxtConfig({
+  alias: {
+    "@gg-style": resolve("./"),
+  },
   // css: ['normalize.css', join(currentDir, './assets/css/index.css')],
 });
