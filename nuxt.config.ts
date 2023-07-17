d504f9e3fa822ad0da2119b395d8aa56a161a87a
Gabriele Leonardi <leonardi.gabriele.lg@gmail.com>
2023-07-17 15:38:39 +0200

    no css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,5 @@ import { dirname, join } from 'path';
 const currentDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineNuxtConfig({
-  css: ['normalize.css', join(currentDir, './assets/css/index.css')],
+  // css: ['normalize.css', join(currentDir, './assets/css/index.css')],
 });
