4a652aa72c5b7565eef79922c3e6d294d6c0290b
ne-u <tommaso@ne-u.xyz>
2023-07-18 15:09:07 +0200

    remove ref

--- plugins/viewport.js
+++ plugins/viewport.js
@@ -4,21 +4,19 @@ function setMq(mq, breakpoints) {
   const breakpointsKeys = Object.keys(breakpoints);
 
   breakpointsKeys.forEach((key) => {
-    mq.value[key] = window.matchMedia(`(min-width: ${breakpoints[key]})`).matches;
+    mq[key] = window.matchMedia(`(min-width: ${breakpoints[key]})`).matches;
   });
 
   return mq;
 }
 
 export default defineNuxtPlugin(async (nuxtApp) => {
-  const mq = ref(
-    Object.keys(tokens.breakpoint).reduce((reducer, key) => {
-      return {
-        ...reducer,
-        [key]: false,
-      };
-    }, {})
-  );
+  let mq = Object.keys(tokens.breakpoint).reduce((reducer, key) => {
+    return {
+      ...reducer,
+      [key]: false,
+    };
+  }, {});
 
   nuxtApp.hook("app:mounted", () => {
     setMq(mq, tokens.breakpoint);
