ac337e032e72266847ba2d820dd6178b44df0c60
ne-u <tommaso@ne-u.xyz>
2023-07-25 15:08:38 +0200

    fix mq loaded

--- plugins/mq.js
+++ plugins/mq.js
@@ -1,12 +1,20 @@
+import { watchOnce } from "@vueuse/core";
+
 export default defineNuxtPlugin(() => {
   const mq = useMq();
-  watchEffect(() => {
-    if (process.client) {
-      nextTick(() => {
-        document.body.classList.add("loaded");
-      });
+  watchOnce(
+    mq,
+    (v) => {
+      if (process.client && Object.values(v).length) {
+        nextTick(() => {
+          document.body.classList.add("loaded");
+        });
+      }
+    },
+    {
+      immediate: true,
     }
-  });
+  );
 
   return {
     provide: {
